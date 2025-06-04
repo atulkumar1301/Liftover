a67dcb6dbaecba64f8d255974227cfdd2bd4ee80
ATUL KUMAR <atul.0298@gmail.com>
2025-06-04 15:06:10 +0200

    Update 2_Liftover_PD.py

--- 2_Liftover_PD.py
+++ 2_Liftover_PD.py
@@ -6,11 +6,11 @@ for i in range (1, 23):
 	data1 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Splitted_CHR/" + str(i) + ".txt", delimiter="\t")
 	data2 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Data/dbSNP_GB_38/All_20180418_" + str(i) + ".vcf.gz", delimiter="\t", compression='gzip', on_bad_lines='skip', comment='#',
 	names=["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"])
-	df = pd.merge(data1, data2, how='left', on='ID')
+	df = pd.merge(data1, data2, how='inner', on='ID')
 	df.to_csv ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Merged_CHR/SNP_Merged_" + str(i) + ".txt", sep="\t", index=False)
 
 # data1 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Splitted_CHR/23.txt", delimiter="\t")
 # data2 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Data/dbSNP_GB_38/All_20180418_X.vcf.gz", delimiter="\t", compression='gzip', on_bad_lines='skip', comment='#',
 # names=["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"])
-# df = pd.merge(data1, data2, how='left', on='ID')
+# df = pd.merge(data1, data2, how='inner', on='ID')
 # df.to_csv ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Merged_CHR/SNP_Merged_X.txt", sep="\t", index=False)
