a17465f567d21254137fce445083482bc602f56c
ATUL KUMAR <atul.0298@gmail.com>
2025-06-04 11:45:34 +0200

    Update 2_Liftover_PD.py

--- 2_Liftover_PD.py
+++ 2_Liftover_PD.py
@@ -8,3 +8,9 @@ for i in range (1, 23):
 	names=["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"])
 	df = pd.merge(data1, data2, how='left', on='ID')
 	df.to_csv ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Merged_CHR/SNP_Merged_" + str(i) + ".txt", sep="\t", index=False)
+
+# data1 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Splitted_CHR/23.txt", delimiter="\t")
+# data2 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Data/dbSNP_GB_38/All_20180418_X.vcf.gz", delimiter="\t", compression='gzip', on_bad_lines='skip', comment='#',
+# names=["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"])
+# df = pd.merge(data1, data2, how='left', on='ID')
+# df.to_csv ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Merged_CHR/SNP_Merged_X.txt", sep="\t", index=False)
