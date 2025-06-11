1aad33eb519a075b659503a83cea3221529f1ae9
ATUL KUMAR <atul.0298@gmail.com>
2025-06-11 17:04:10 +0300

    Update 1_Splitting_File_Chromosome_wise.py

--- 1_Splitting_File_Chromosome_wise.py
+++ 1_Splitting_File_Chromosome_wise.py
@@ -1,7 +1,7 @@
 for i in range (1, 26):
-    f_m = open ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/BC_h550/Splitted_CHR/" + str(i) + ".txt", 'w', 1)
+    f_m = open ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/Merged_FinnHap550_BC_h550/Splitted_CHR/" + str(i) + ".txt", 'w', 1)
     f_m.write ("Chr" + "\t" + "ID" + "\t" + "CM" + "\t" + "Pos" + "\t" + "Alt" + "\t" + "Ref" + "\n")
-    with open ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/BC_h550/BC_h550_g05_hw0001.bim", 'r') as bimfile:
+    with open ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/Merged_FinnHap550_BC_h550/Merged_FinnHap550_BC_h550.bim", 'r') as bimfile:
         for line in bimfile:
             line_list = line.split ("\t")
             if (int (line_list [0]) == i):
