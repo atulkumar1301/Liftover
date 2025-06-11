5fc76c7c62190e78e6909a062075664fc45c62cc
ATUL KUMAR <atul.0298@gmail.com>
2025-06-11 16:54:25 +0300

    Update 1_Splitting_File_Chromosome_wise.py

--- 1_Splitting_File_Chromosome_wise.py
+++ 1_Splitting_File_Chromosome_wise.py
@@ -1,6 +1,7 @@
 for i in range (1, 26):
-    f_m = open ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Splitted_CHR/" + str(i) + ".txt", 'w', 1)
-    with open ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/FinnHap550_postQC-9Sept09.bim", 'r') as bimfile:
+    f_m = open ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/BC_h550/Splitted_CHR/" + str(i) + ".txt", 'w', 1)
+    f_m.write ("Chr" + "\t" + "ID" + "\t" + "CM" + "\t" + "Pos" + "\t" + "Alt" + "\t" + "Ref" + "\n")
+    with open ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/BC_h550/BC_h550_g05_hw0001.bim", 'r') as bimfile:
         for line in bimfile:
             line_list = line.split ("\t")
             if (int (line_list [0]) == i):
