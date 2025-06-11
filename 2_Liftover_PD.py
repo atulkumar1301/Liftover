ce1ac5d657026d6311aabf900937e22e12cd53c1
ATUL KUMAR <atul.0298@gmail.com>
2025-06-11 17:34:10 +0300

    Update 2_Liftover_PD.py

--- 2_Liftover_PD.py
+++ 2_Liftover_PD.py
@@ -3,14 +3,14 @@ import zipfile
 import pandas as pd
 # reading csv files
 for i in range (1, 23):
-	data1 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Splitted_CHR/" + str(i) + ".txt", delimiter="\t")
-	data2 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Data/dbSNP_GB_38/All_20180418_" + str(i) + ".vcf.gz", delimiter="\t", compression='gzip', on_bad_lines='skip', comment='#',
-	names=["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"])
-	df = pd.merge(data1, data2, how='inner', on='ID')
-	df.to_csv ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Merged_CHR/SNP_Merged_" + str(i) + ".txt", sep="\t", index=False)
+    data1 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/Merged_FinnHap550_BC_h550/Splitted_CHR/" + str(i) + ".txt", delimiter="\t")
+    data2 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Data/dbSNP_GB_38/All_20180418_" + str(i) + ".vcf.gz", delimiter="\t", compression='gzip', on_bad_lines='skip', comment='#',
+    names=["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"])
+    df = pd.merge(data1, data2, how='inner', on='ID')
+    df.to_csv ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/Merged_FinnHap550_BC_h550/Merged_CHR/SNP_Merged_" + str(i) + ".txt", sep="\t", index=False)
 
-# data1 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Splitted_CHR/23.txt", delimiter="\t")
-# data2 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Data/dbSNP_GB_38/All_20180418_X.vcf.gz", delimiter="\t", compression='gzip', on_bad_lines='skip', comment='#',
+# data1 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/Merged_FinnHap550_BC_h550/Splitted_CHR/24.txt", delimiter="\t")
+# data2 = pd.read_csv("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Data/dbSNP_GB_38/All_20180418_Y.vcf.gz", delimiter="\t", compression='gzip', on_bad_lines='skip', comment='#',
 # names=["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"])
 # df = pd.merge(data1, data2, how='inner', on='ID')
-# df.to_csv ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/FinnHap550/Merged_CHR/SNP_Merged_X.txt", sep="\t", index=False)
+# df.to_csv ("/Users/akumar/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Projects/Genetic_Data_QC/Merged_FinnHap550_BC_h550/Merged_CHR/SNP_Merged_Y.txt", sep="\t", index=False)
